aa4956cae59871fcec3131109704cc9c8f346a4b
Alejandro Guirao <lekumberri@gmail.com>
2016-03-01 09:11:43 +0100

    Add ReadFile example

---
+++ src/gopl.io/ch1/dup3/main2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+
+		for line, reps := range counts {
+			if reps > 1 && line != "" {
+				fmt.Printf("%d\t%s\n", reps, line)
+			}
+		}
+	}
+}
