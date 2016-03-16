9ce4efe2d1721bacedb1f1ef0d8757eb35577062
Alejandro Guirao <lekumberri@gmail.com>
2016-03-16 08:49:29 +0100

    Add utf8 code

--- src/gopl.io/ch3/stringexamples/main.go
+++ src/gopl.io/ch3/stringexamples/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	s := "Feliz Año nuevo"
-	fmt.Println(len(s))
+	fmt.Println(s)
+	fmt.Println("Length:", len(s))
+	fmt.Println("Rule count:", utf8.RuneCountInString(s))
 	for ix, v := range s {
-		fmt.Printf("%d -> %c\n", ix, v)
+		fmt.Printf("%d -> %q, %d\n", ix, v, v)
+	}
+	t := "Feliz \xe4\xb8\x96\xe7\x95\x8c"
+	fmt.Println(t)
+	fmt.Println(len(t))
+	fmt.Println("Rule count:", utf8.RuneCountInString(t))
+	for ix, v := range t {
+		fmt.Printf("%d -> %q, %d\n", ix, v, v)
 	}
 }
