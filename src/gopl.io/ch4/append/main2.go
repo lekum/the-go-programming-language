2cbda6201db149123525e807c62462df031e6c20
Alejandro Guirao <lekumberri@gmail.com>
2016-03-30 09:33:14 +0200

    Change for variadic appendInts

--- src/gopl.io/ch4/append/main2.go
+++ src/gopl.io/ch4/append/main2.go
@@ -23,6 +23,28 @@ func appendInt(x []int, i int) (y []int) {
 	return z
 }
 
+func appendInts(x []int, is ...int) (y []int) {
+	var z []int
+	xlen := len(x)
+	zlen := xlen + len(is)
+	xcap := cap(x)
+	if zlen < xcap {
+		// Enough space in x
+		z = x[:zlen]
+	} else {
+		// Grow x by doubling
+		zcap := zlen
+		if zcap < 2*xlen {
+			zcap = 2 * xlen
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[xlen:], is)
+	return z
+
+}
+
 func main() {
 	var x, y []int
 	for i := 0; i < 10; i++ {
@@ -30,4 +52,6 @@ func main() {
 		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
 		x = y
 	}
+	a := []int{1, 2, 3}
+	fmt.Println(appendInts(a, 4, 5, 6))
 }
