df4d70bfcd55b09eadfff90f22b69ac15c361b67
Alejandro Guirao <lekumberri@gmail.com>
2016-04-21 11:42:18 +0200

    Add findlinks2

--- src/gopl.io/ch5/findlinks2/main.go
+++ src/gopl.io/ch5/findlinks2/main.go
@@ -1,24 +1,13 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 125.
-
-// Findlinks2 does an HTTP GET on each URL, parses the
-// result as HTML, and prints the links within it.
-//
-// Usage:
-//	findlinks url ...
+// Prints the links in an HTML document read from standard input
 package main
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
 	"net/http"
 	"os"
-
-	"golang.org/x/net/html"
 )
 
-// visit appends to links each link found in n, and returns the result.
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
@@ -33,22 +22,6 @@ func visit(links []string, n *html.Node) []string {
 	return links
 }
 
-//!+
-func main() {
-	for _, url := range os.Args[1:] {
-		links, err := findLinks(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
-			continue
-		}
-		for _, link := range links {
-			fmt.Println(link)
-		}
-	}
-}
-
-// findLinks performs an HTTP GET request for url, parses the
-// response as HTML, and extracts and returns the links.
 func findLinks(url string) ([]string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -64,6 +37,19 @@ func findLinks(url string) ([]string, error) {
 		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
 	}
 	return visit(nil, doc), nil
+
 }
 
-//!-
+func main() {
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
