8c9d5d496200486b68fdfb0e1692d46b7e5b4485
Alejandro Guirao <lekumberri@gmail.com>
2016-04-22 09:22:43 +0200

    Add first version of WaitForServer

---
+++ src/gopl.io/ch5/wait/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func WaitForServer(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
+		if err == nil {
+			return nil // sucess
+		}
+		log.Printf("server not responding (%s); retrying...", err)
+		time.Sleep(time.Second << uint(tries)) // exponential back-off
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
+}
+
+func main() {
+	url := os.Args[1]
+	if err := WaitForServer(url); err != nil {
+		log.Fatalf("Site is down: %v\n", err)
+	}
+}
