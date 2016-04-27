1c65c34b6671b9726f46f721b4ae04fabc3a557c
Alejandro Guirao <lekumberri@gmail.com>
2016-04-27 08:49:45 +0200

    Add example of anonymous function (II)

--- src/gopl.io/ch5/toposort/main.go
+++ src/gopl.io/ch5/toposort/main.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 136.
-
-// The toposort program prints the nodes of a DAG in topological order.
 package main
 
 import (
@@ -11,18 +5,14 @@ import (
 	"sort"
 )
 
-//!+table
-// prereqs maps computer science courses to their prerequisites.
 var prereqs = map[string][]string{
 	"algorithms": {"data structures"},
 	"calculus":   {"linear algebra"},
-
 	"compilers": {
 		"data structures",
 		"formal languages",
 		"computer organization",
 	},
-
 	"data structures":       {"discrete math"},
 	"databases":             {"data structures"},
 	"discrete math":         {"intro to programming"},
@@ -32,20 +22,18 @@ var prereqs = map[string][]string{
 	"programming languages": {"data structures", "computer organization"},
 }
 
-//!-table
-
-//!+main
 func main() {
 	for i, course := range topoSort(prereqs) {
 		fmt.Printf("%d:\t%s\n", i+1, course)
 	}
+
 }
 
+// depth first search
 func topoSort(m map[string][]string) []string {
 	var order []string
 	seen := make(map[string]bool)
 	var visitAll func(items []string)
-
 	visitAll = func(items []string) {
 		for _, item := range items {
 			if !seen[item] {
@@ -55,15 +43,11 @@ func topoSort(m map[string][]string) []string {
 			}
 		}
 	}
-
 	var keys []string
 	for key := range m {
 		keys = append(keys, key)
 	}
-
 	sort.Strings(keys)
 	visitAll(keys)
 	return order
 }
-
-//!-main
