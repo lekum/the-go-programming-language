303d7527f10ee9e78ad0d09626f5d65531d501eb
Alejandro Guirao <lekumberri@gmail.com>
2016-04-29 09:04:47 +0200

    Add exercise 5_10

--- src/gopl.io/ch5/toposort/main.go
+++ src/gopl.io/ch5/toposort/main.go
@@ -23,7 +23,12 @@ var prereqs = map[string][]string{
 }
 
 func main() {
+	fmt.Println("topoSort:")
 	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+	fmt.Println("topoSort2:")
+	for i, course := range topoSort2(prereqs) {
 		fmt.Printf("%d:\t%s\n", i+1, course)
 	}
 
@@ -48,6 +53,31 @@ func topoSort(m map[string][]string) []string {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
+
+func topoSort2(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items map[string]bool)
+	visitAll = func(items map[string]bool) {
+		for item, _ := range items {
+			if !seen[item] {
+				seen[item] = true
+				map2 := make(map[string]bool)
+				for _, key2 := range m[item] {
+					map2[key2] = true
+				}
+				visitAll(map2)
+				order = append(order, item)
+			}
+		}
+	}
+	keys := make(map[string]bool)
+	for key := range m {
+		keys[key] = true
+	}
 	visitAll(keys)
 	return order
 }
