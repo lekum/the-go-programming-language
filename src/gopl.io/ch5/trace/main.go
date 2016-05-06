35c2576d54c5dbbcf4ed1ba4d8a475e319066617
Alejandro Guirao <lekumberri@gmail.com>
2016-05-06 08:47:40 +0200

    Add another defer example

--- src/gopl.io/ch5/trace/main.go
+++ src/gopl.io/ch5/trace/main.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 146.
-
-// The trace program uses defer to add entry/exit diagnostics to a function.
 package main
 
 import (
@@ -11,10 +5,8 @@ import (
 	"time"
 )
 
-//!+main
 func bigSlowOperation() {
-	defer trace("bigSlowOperation")() // don't forget the extra parentheses
-	// ...lots of work...
+	defer trace("bigSlowOperation")()
 	time.Sleep(10 * time.Second) // simulate slow operation by sleeping
 }
 
@@ -24,17 +16,6 @@ func trace(msg string) func() {
 	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
 }
 
-//!-main
-
 func main() {
 	bigSlowOperation()
 }
-
-/*
-!+output
-$ go build gopl.io/ch5/trace
-$ ./trace
-2015/11/18 09:53:26 enter bigSlowOperation
-2015/11/18 09:53:36 exit bigSlowOperation (10.000589217s)
-!-output
-*/
