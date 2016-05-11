e84aa2f0d7dd303414dc6e0f13c77de9c71cadf5
Alejandro Guirao <lekumberri@gmail.com>
2016-05-11 09:34:43 +0200

    Add example of panic and defer

--- src/gopl.io/ch5/defer1/defer.go
+++ src/gopl.io/ch5/defer1/defer.go
@@ -1,17 +1,8 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 150.
-
-// Defer1 demonstrates a deferred call being invoked during a panic.
 package main
 
-import "fmt"
-
-//!+f
-func main() {
-	f(3)
-}
+import (
+	"fmt"
+)
 
 func f(x int) {
 	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
@@ -19,30 +10,6 @@ func f(x int) {
 	f(x - 1)
 }
 
-//!-f
-
-/*
-//!+stdout
-f(3)
-f(2)
-f(1)
-defer 1
-defer 2
-defer 3
-//!-stdout
-
-//!+stderr
-panic: runtime error: integer divide by zero
-main.f(0)
-        src/gopl.io/ch5/defer1/defer.go:14
-main.f(1)
-        src/gopl.io/ch5/defer1/defer.go:16
-main.f(2)
-        src/gopl.io/ch5/defer1/defer.go:16
-
-main.f(3)
-        src/gopl.io/ch5/defer1/defer.go:16
-main.main()
-        src/gopl.io/ch5/defer1/defer.go:10
-//!-stderr
-*/
+func main() {
+	f(4)
+}
