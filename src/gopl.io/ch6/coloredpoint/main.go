c9185c319dbde5e5a96bf48deda75abc20c9a67f
Alejandro Guirao <lekumberri@gmail.com>
2016-06-06 08:55:17 +0200

    Add pointer in the struct

--- src/gopl.io/ch6/coloredpoint/main.go
+++ src/gopl.io/ch6/coloredpoint/main.go
@@ -7,23 +7,17 @@ import (
 )
 
 type ColoredPoint struct {
-	geometry.Point
+	*geometry.Point
 	Color color.RGBA
 }
 
 func main() {
-	var cp ColoredPoint
-	cp.X = 1
-	fmt.Println(cp.X)
-	cp.Y = 2
-	fmt.Println(cp.Y)
-
 	red := color.RGBA{255, 0, 0, 255}
 	blue := color.RGBA{0, 0, 255, 255}
-	var p = ColoredPoint{geometry.Point{1, 1}, red}
-	var q = ColoredPoint{geometry.Point{5, 4}, blue}
-	fmt.Println(p.Distance(q.Point))
-	p.ScaleBy(2.0)
-	q.ScaleBy(2.0)
-	fmt.Println(p.Distance(q.Point))
+	p := ColoredPoint{&geometry.Point{1, 1}, red}
+	q := ColoredPoint{&geometry.Point{5, 4}, blue}
+	fmt.Println(p.Distance(*q.Point))
+	q.Point = p.Point
+	p.ScaleBy(2)
+	fmt.Println(*p.Point, *q.Point)
 }
